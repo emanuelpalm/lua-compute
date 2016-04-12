4af8b17a69bbfa13027b721a17e593e64798db69
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-12 11:39:01 +0200

    Update setup.sh comments

--- build/bash/setup.sh
+++ build/bash/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-### A collection of routines for setting up your local repository for building
-### and development. Prints nothing if no action is performed.
+# A collection of routines for setting up your local repository for building
+# and development. Prints nothing if no action is performed.
 
-## Standard functions.
+# Standard functions.
 abspath() {
     if [ -d "$(dirname "$1")" ];
     then
@@ -14,11 +14,11 @@ absdirname() {
     echo "$(dirname $(abspath "$1"))"
 }
 
-## Standard variables.
+# Standard variables.
 PROJECT_ROOT=$(dirname "$(dirname "$(absdirname "$0")")")
 PROJECT_BUILD="$PROJECT_ROOT/build"
 PROJECT_BASH="$PROJECT_BUILD/bash"
 
-## Modules.
+# Modules.
 . "$PROJECT_BASH/setup_tools.sh"
 . "$PROJECT_BASH/setup_env.sh"
