ef53558d104fb2f0c1c361f5282f39e49cfd0747
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-12 16:52:47 +0200

    Refine native.mk

--- build/make/native.mk
+++ build/make/native.mk
@@ -2,18 +2,27 @@
 
 NATIVE_OUT  := ${PROJECT_OUT}/native
 LIB_LUAMARE := ${NATIVE_OUT}/libs/luamare/libluamare.so
-LIB_LUAJIT2 := ${NATIVE_OUT}/libs/luajit2/lib/libluajit-5.1.a
+LIB_LUAJIT2 := ${NATIVE_OUT}/libs/luajit2/lib/libluajit-5.1.so
 
-default-native: ${LIB_LUAMARE}
+# User actions.
 
-all-native: ${LIB_LUAMARE}
+default-native: libs-native
 
-${LIB_LUAMARE}: ${LIB_LUAJIT2}
+all-native: libs-native
 
-${LIB_LUAJIT2}:
-	${PROJECT_BUILD}/bash/make_luajit2.sh ${PROJECT_BUILD}/env.conf native
+libs-native:
+	@${MAKE} -f ${PROJECT_MAKE}/main.mk libs-native-inner --no-print-directory
 
 clean-native:
 	${RM} -R ${NATIVE_OUT}
 
-.PHONY: default-native all-native clean-native
+.PHONY: default-native all-native libs-native clean-native
+
+# Inner actions. Do not use these directly.
+
+libs-native-inner: ${LIB_LUAJIT2}
+
+${LIB_LUAJIT2}:
+	${PROJECT_BUILD}/bash/make_luajit2.sh ${PROJECT_BUILD}/env.conf native
+
+.PHONY: libs-native-inner
