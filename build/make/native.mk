00bfdb58ac3e3ec33df64bd3f50acc0cc23b7859
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-12 15:31:33 +0200

    Make native.mk build LuaJIT2

--- build/make/native.mk
+++ build/make/native.mk
@@ -1,9 +1,19 @@
 # Makefile for native (current platform) build targets.
 
-default-native:
+NATIVE_OUT  := ${PROJECT_OUT}/native
+LIB_LUAMARE := ${NATIVE_OUT}/libs/luamare/libluamare.so
+LIB_LUAJIT2 := ${NATIVE_OUT}/libs/luajit2/lib/libluajit-5.1.a
 
-all-native:
+default-native: ${LIB_LUAMARE}
+
+all-native: ${LIB_LUAMARE}
+
+${LIB_LUAMARE}: ${LIB_LUAJIT2}
+
+${LIB_LUAJIT2}:
+	${PROJECT_BUILD}/bash/make_luajit2.sh ${PROJECT_BUILD}/env.conf native
 
 clean-native:
+	${RM} -R ${NATIVE_OUT}
 
 .PHONY: default-native all-native clean-native
