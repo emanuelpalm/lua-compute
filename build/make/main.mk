bc407fc579c72f63a25df573dcb7578d734cdfa9
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-12 15:31:12 +0200

    Re-order entries in main.mk to avoid bad default make rule

--- build/make/main.mk
+++ build/make/main.mk
@@ -5,12 +5,12 @@ PROJECT_MAKE := $(dir $(lastword $(MAKEFILE_LIST)))
 
 RM = rm -f
 
--include ${PROJECT_MAKE}../env.conf
-include ${PROJECT_MAKE}/setup.mk
-include ${PROJECT_MAKE}/native.mk
-
 default: default-setup default-native
 all: all-setup all-native
 clean: clean-setup clean-native
 
 .PHONY: default all clean
+
+-include ${PROJECT_MAKE}../env.conf
+include ${PROJECT_MAKE}/setup.mk
+include ${PROJECT_MAKE}/native.mk
