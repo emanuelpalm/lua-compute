2755d8eeef2d5c64d68ce26b79a0dbf58b6e8750
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-12 14:29:30 +0200

    Make build_luajit2.sh build for Android

--- build/bash/build_luajit2.sh
+++ build/bash/build_luajit2.sh
@@ -24,17 +24,74 @@ PLATFORM=${2:-native}
 
 LUA_PATH="$PROJECT_LIBS/luajit2"
 
+# Builds for Android.
+android() {
+    local filename=$(basename "$1")
+    local name=${filename:5}
+    echo >&2 "Building LuaJIT2 for Android ($name) ..."
+
+    # If name contains 64, we assume it names a 64-bit architecture.
+    if [[ $name == *64* ]];
+    then
+        local cpu="64"
+    else
+        local cpu="32"
+    fi
+
+    local toolchains=$(find $NDK_PATH/toolchains -maxdepth 1 -ipath *$name*)
+    for i in $toolchains;
+    do
+        # 64-bit architectures must have 64 in their toolchain names, and
+        # 32-bit architectures must not have 64 in their names.
+        if [ "$cpu" == "32" ];
+        then
+            [[ $i == *64* ]] && { continue; }
+        else
+            [[ $i != *64* ]] && { continue; }
+        fi
+        local toolchain=$i
+        break
+    done
+    if [ "$toolchain" == "" ];
+    then
+        echo >&2 "[!] No suitable NDK toolchain available for arch ($name)."
+    else
+        local gcc=$(find $toolchain -ipath */bin/*-gcc)
+        local bin_prefix=${gcc%???}
+        local lua_prefix="$PROJECT_OUT/android-$name/libs/luajit2"
+        local uname=`uname`
+        set -x
+        mkdir -p "$lua_prefix"
+        cd "$LUA_PATH" \
+            && $MAKE CC=gcc HOST_CC="gcc -m$cpu" HOST_SYS=$uname CROSS=$bin_prefix TARGET_FLAGS="--sysroot=$1" TARGET_SYS=Linux \
+            && $MAKE install PREFIX=\""$lua_prefix"\" \
+            && $MAKE clean
+        set +x
+    fi
+}
+
+# Builds for the host platform.
+native() {
+    echo >&2 "Building LuaJIT2 ..."
+    local lua_prefix="$PROJECT_OUT/native/libs/luajit2"
+    mkdir -p "$lua_prefix"
+    cd "$LUA_PATH" \
+        && $MAKE \
+        && $MAKE install PREFIX=\""$lua_prefix"\" \
+        && $MAKE clean
+}
+
 case $PLATFORM in
     android)
-        echo "Android: $NDK_PATH $LUA_PATH"
+        archs=${NDK_TARGET_ARCHS//:/ }
+        for arch in $archs;
+        do
+            android $arch
+        done
+        unset archs
         ;;
     native)
-        LUA_PREFIX="$PROJECT_OUT/native/libs/luajit2"
-        mkdir -p "$LUA_PREFIX"
-        cd "$LUA_PATH" \
-            && make \
-            && make install PREFIX=\""$LUA_PREFIX"\" \
-            && make clean
+        native
         ;;
     *)
         echo >&2 "[!] Unsupported target platform: $PLATFORM."
