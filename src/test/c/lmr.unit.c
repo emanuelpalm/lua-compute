2caba90a9bb0a1e49d84bf2b76affa806ff46645
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-14 16:59:44 +0200

    Add lua state provider to lmr.unit.c

--- src/test/c/lmr.unit.c
+++ src/test/c/lmr.unit.c
@@ -1,13 +1,25 @@
 #include "../../main/c/lmr.h"
 #include "unit.h"
+#include <lauxlib.h>
+#include <lua.h>
+
+void provider_lua_state(unit_T *T, unit_TestFunction t) {
+    lua_State *L = luaL_newstate();
+    if (L == NULL) {
+        unit_fatal(T, "Failed to create new Lua state object.");
+    }
+    t(T, L);
+    lua_close(L);
+}
 
 // Test cases.
 void test_dummy(unit_T *T, void *arg);
 
 void suite_lmr(unit_T *T) {
-    unit_run_test(T, &test_dummy, NULL);
+    unit_run_test(T, &test_dummy, provider_lua_state);
 }
 
 void test_dummy(unit_T *T, void *arg) {
+    lua_State *L = arg;
     // Do nothing.
 }
