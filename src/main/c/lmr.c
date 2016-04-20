a57def9d1edd6426b3b65cecbd2418d32b8c4eb3
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-20 14:56:51 +0200

    Re-implement lmr_register()

--- src/main/c/lmr.c
+++ src/main/c/lmr.c
@@ -2,8 +2,6 @@
 #include "lmrlua.h"
 #include <stdlib.h>
 
-static const char* lmr_register_key = "process_key";
-
 LMR_API void lmr_openlib(lua_State* L, const lmr_Config* c)
 {
     static const luaL_Reg functions[] = {
@@ -21,19 +19,29 @@ LMR_API int lmr_register(lua_State* L, const lmr_Job j)
         const char* buffer = j.program.lua;
         const size_t size = j.program.length;
         int status;
-        if ((status = luaL_loadbuffer(L, buffer, size, "lmr_job")) != 0) {
+
+        status = luaL_loadbuffer(L, buffer, size, "lmr_job");
+        if (status != 0) {
             return status;
         }
-        if ((status = lua_pcall(L, 0, 0, 0)) != 0) {
+
+        lua_pushinteger(L, j.job_id);
+        lua_setglobal(L, "lmr");
+
+        status = lua_pcall(L, 0, 0, 0);
+
+        lua_pushnil(L);
+        lua_setglobal(L, "lmr");
+
+        if (status != 0) {
             return status;
         }
     }
     // Ensure that the executed job actually called `lmr:job()`.
     {
-        lua_pushlightuserdata(L, (void*)lmr_register_key);
+        lua_pushfstring(L, "lmr_job_%u", j.job_id);
         lua_gettable(L, LUA_REGISTRYINDEX);
-        const int type = lua_type(L, -1);
-        if (type != LUA_TNUMBER || lua_tointeger(L, -1) != j.job_id) {
+        if (lua_type(L, -1) != LUA_TFUNCTION) {
             return LMR_ERRNOCALL;
         }
     }
