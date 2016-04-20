10d5bd873dcee24c60ad92e39284a41f9bdbc10b
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-20 11:19:56 +0200

    Implement lmr_openlib() in lmr.c

--- src/main/c/lmr.c
+++ src/main/c/lmr.c
@@ -1,8 +1,18 @@
+#define LUA_COMPAT_MODULE
+
 #include "lmr.h"
 #include "lmrlua.h"
 #include <stdlib.h>
 
-LMR_API void lmr_openlib(lua_State* L, const lmr_Config* c) { return -1; }
+LMR_API void lmr_openlib(lua_State* L, const lmr_Config* c)
+{
+    static const luaL_Reg functions[] = {
+        {.name = "job", .func = lmr_l_job },
+        {.name = "log", .func = lmr_l_log },
+        {.name = NULL, .func = NULL },
+    };
+    luaL_register(L, "lmr", functions);
+}
 
 LMR_API int lmr_register(lua_State* L, const lmr_Job j) { return -1; }
 
