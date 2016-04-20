80dbfa1b3397c6838597c116ce2475ed2ae2be8e
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-20 10:32:28 +0200

    Add test_process() test case to lmr.unit.c

--- src/test/c/lmr.unit.c
+++ src/test/c/lmr.unit.c
@@ -22,10 +22,12 @@ void provider_lua_state(unit_T* T, unit_TestFunction t)
 
 // Test cases.
 void test_log(unit_T* T, void* arg);
+void test_process(unit_T* T, void* arg);
 
 void suite_lmr(unit_T* T)
 {
     unit_run_test(T, test_log, provider_lua_state);
+    unit_run_test(T, test_process, provider_lua_state);
 }
 
 // Used to test `lmr:log()` lua function.
@@ -80,4 +82,44 @@ void test_log(unit_T* T, void* arg)
     unit_assert(T, logf_job_id == 1);
     unit_assert(T, logf_batch_id == 2);
     unit_assert(T, strcmp(logf_s, "hello") == 0);
+}
+
+void test_process(unit_T* T, void* arg)
+{
+    lua_State* L = arg;
+
+    const lmr_Config c = {
+        .log_function = logf,
+    };
+    const lmr_Job j = {
+        .job_id = 2,
+        .program = {
+            .lua = "lmr:job(function (batch) return batch:upper() end)",
+            .length = 50,
+        },
+    };
+    const lmr_Batch b_in = {
+        .job_id = 2,
+        .batch_id = 1,
+        .data = {
+            .bytes = (uint8_t*)"hello",
+            .length = 5,
+        },
+    };
+    lmr_Batch b_out = {
+        .job_id = 0,
+        .batch_id = 0,
+        .data = {
+            .bytes = (uint8_t*)"",
+            .length = 0,
+        },
+    };
+    unit_assert(T, lmr_openlib(L, &c) == 0);
+    unit_assert(T, lmr_register(L, j) == 0);
+    unit_assert(T, lmr_process(L, b_in, &b_out) == 0);
+
+    unit_assert(T, b_out.job_id == 2);
+    unit_assert(T, b_out.batch_id == 1);
+    unit_assert(T, memcmp(b_out.data.bytes, "HELLO", 5) == 0);
+    unit_assert(T, b_out.data.length == 5);
 }
