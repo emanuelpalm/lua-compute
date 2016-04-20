3b58efb47655ef01eca8a28d5bc98170b585afd2
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-20 10:19:38 +0200

    Add test_log() test case to lmr.unit.c

--- src/test/c/lmr.unit.c
+++ src/test/c/lmr.unit.c
@@ -2,6 +2,13 @@
 #include "unit.h"
 #include <lauxlib.h>
 #include <lua.h>
+#include <string.h>
+
+//{ Data registered during calls to `logf()`.
+static uint32_t logf_job_id = 0;
+static uint32_t logf_batch_id = 0;
+static const char* logf_s = "";
+//}
 
 void provider_lua_state(unit_T* T, unit_TestFunction t)
 {
@@ -14,15 +21,63 @@ void provider_lua_state(unit_T* T, unit_TestFunction t)
 }
 
 // Test cases.
-void test_dummy(unit_T* T, void* arg);
+void test_log(unit_T* T, void* arg);
 
 void suite_lmr(unit_T* T)
 {
-    unit_run_test(T, &test_dummy, provider_lua_state);
+    unit_run_test(T, test_log, provider_lua_state);
 }
 
-void test_dummy(unit_T* T, void* arg)
+// Used to test `lmr:log()` lua function.
+static void logf(const uint32_t job_id, const uint32_t batch_id, const char* s)
+{
+    logf_job_id = job_id;
+    logf_batch_id = batch_id;
+    logf_s = s;
+}
+
+void test_log(unit_T* T, void* arg)
 {
     lua_State* L = arg;
-    // Do nothing.
+
+    const lmr_Config c = {
+        .log_function = logf,
+    };
+    const lmr_Job j = {
+        .job_id = 1,
+        .program = {
+            .lua = "lmr:job(function (batch) lmr:log(batch) end)",
+            .length = 44,
+        },
+    };
+    const lmr_Batch b_in = {
+        .job_id = 1,
+        .batch_id = 2,
+        .data = {
+            .bytes = (uint8_t*)"hello",
+            .length = 5,
+        },
+    };
+    lmr_Batch b_out = {
+        .job_id = 0,
+        .batch_id = 0,
+        .data = {
+            .bytes = NULL,
+            .length = 0,
+        },
+    };
+    unit_assert(T, lmr_openlib(L, &c) == 0);
+    unit_assert(T, lmr_register(L, j) == 0);
+    unit_assert(T, lmr_process(L, b_in, &b_out) == 0);
+
+    //{ Since `lmr:report()` was never called, the out batch must be untouched.
+    unit_assert(T, b_out.job_id == 0);
+    unit_assert(T, b_out.batch_id == 0);
+    unit_assert(T, b_out.data.bytes == NULL);
+    unit_assert(T, b_out.data.length == 0);
+    //}
+
+    unit_assert(T, logf_job_id == 1);
+    unit_assert(T, logf_batch_id == 2);
+    unit_assert(T, strcmp(logf_s, "hello") == 0);
 }
